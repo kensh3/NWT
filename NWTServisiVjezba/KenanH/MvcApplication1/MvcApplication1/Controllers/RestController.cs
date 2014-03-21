5d9ffd0f755ed9297709a1f8e42bb27319dd9391
kensh3 <kensh90@gmail.com>
2014-03-21 13:51:27 +0100

    Ubacni servisi

---
+++ NWTServisiVjezba/KenanH/MvcApplication1/MvcApplication1/Controllers/RestController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MvcApplication1.Controllers
+{
+    public class RestController : ApiController
+    {
+
+        // GET api/rest
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Bezvezna metoda", "ne primam nista, vracam gluposti", "bla", "bla" };
+        }
+
+        // GET api/rest/zbir
+        public int Get(int a,int b)
+        {
+             return a + b;
+        }
+
+        // GET api/rest/5
+        public string Get(int id)
+        {
+            if (id == 1) return "Vraćam prvog studenta.";
+            if (id == 2) return "Vraćam drugog studenta.";
+            else return "Ne znam ni ja šta vraćam.";
+        }
+
+        // POST api/rest
+        public string Post([FromBody]string value)
+        {
+
+            return "Unijeli ste: " +  value;
+        }
+
+
+
+    }
+}

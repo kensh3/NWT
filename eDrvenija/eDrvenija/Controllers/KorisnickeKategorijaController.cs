e88fb5e6f56e8451dbdc822eb4ab46bfab2070f1
kensh3 <kensh90@gmail.com>
2014-03-21 14:53:35 +0100

    Scaffolding
    Dokumenti, korisnickeKategorije, statusi

---
+++ eDrvenija/eDrvenija/Controllers/KorisnickeKategorijaController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using eDrvenija.eDrvenija.Models;
+
+namespace eDrvenija.eDrvenija.Controllers
+{
+    public class KorisnickeKategorijaController : ApiController
+    {
+        private edrvenijabazaEntities2 db = new edrvenijabazaEntities2();
+
+        // GET api/KorisnickeKategorija
+        public IEnumerable<korisnicikategorije> Getkorisnicikategorijes()
+        {
+            var korisnicikategorije = db.korisnicikategorije.Include(k => k.kategorije).Include(k => k.korisnici);
+            return korisnicikategorije.AsEnumerable();
+        }
+
+        // GET api/KorisnickeKategorija/5
+        public korisnicikategorije Getkorisnicikategorije(int id)
+        {
+            korisnicikategorije korisnicikategorije = db.korisnicikategorije.Find(id);
+            if (korisnicikategorije == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+
+            return korisnicikategorije;
+        }
+
+        // PUT api/KorisnickeKategorija/5
+        public HttpResponseMessage Putkorisnicikategorije(int id, korisnicikategorije korisnicikategorije)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+
+            if (id != korisnicikategorije.idKorisnikaKategorije)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            db.Entry(korisnicikategorije).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        // POST api/KorisnickeKategorija
+        public HttpResponseMessage Postkorisnicikategorije(korisnicikategorije korisnicikategorije)
+        {
+            if (ModelState.IsValid)
+            {
+                db.korisnicikategorije.Add(korisnicikategorije);
+                db.SaveChanges();
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, korisnicikategorije);
+                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = korisnicikategorije.idKorisnikaKategorije }));
+                return response;
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+        }
+
+        // DELETE api/KorisnickeKategorija/5
+        public HttpResponseMessage Deletekorisnicikategorije(int id)
+        {
+            korisnicikategorije korisnicikategorije = db.korisnicikategorije.Find(id);
+            if (korisnicikategorije == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            db.korisnicikategorije.Remove(korisnicikategorije);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, korisnicikategorije);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ eDrvenija/eDrvenija/Controllers/StatusiController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using eDrvenija.eDrvenija.Models;
+
+namespace eDrvenija.eDrvenija.Controllers
+{
+    public class StatusiController : ApiController
+    {
+        private edrvenijabazaEntities2 db = new edrvenijabazaEntities2();
+
+        // GET api/Statusi
+        public IEnumerable<statusi> Getstatusis()
+        {
+            var statusi = db.statusi.Include(s => s.korisnici);
+            return statusi.AsEnumerable();
+        }
+
+        // GET api/Statusi/5
+        public statusi Getstatusi(int id)
+        {
+            statusi statusi = db.statusi.Find(id);
+            if (statusi == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+
+            return statusi;
+        }
+
+        // PUT api/Statusi/5
+        public HttpResponseMessage Putstatusi(int id, statusi statusi)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+
+            if (id != statusi.idStatusa)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            db.Entry(statusi).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        // POST api/Statusi
+        public HttpResponseMessage Poststatusi(statusi statusi)
+        {
+            if (ModelState.IsValid)
+            {
+                db.statusi.Add(statusi);
+                db.SaveChanges();
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, statusi);
+                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = statusi.idStatusa }));
+                return response;
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+        }
+
+        // DELETE api/Statusi/5
+        public HttpResponseMessage Deletestatusi(int id)
+        {
+            statusi statusi = db.statusi.Find(id);
+            if (statusi == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            db.statusi.Remove(statusi);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, statusi);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
